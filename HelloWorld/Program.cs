79221abf4310fa179fd749d405dc068a83ec460a
Opilane TTHK <opilane@tthk.ee>
2021-11-09 13:58:54 +0200

    adds string interpolation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,7 +12,18 @@ namespace HelloWorld
             Console.WriteLine("Enter your first name: ");
             string firstname = Console.ReadLine();
 
-            Console.WriteLine("Hello " + firstname + "!");
+            //program asks the user to enter their first name
+            //program asks the user using their first name
+
+            Console.WriteLine("Enter your last name: ");
+            string lastname = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + firstname + "!");
+
+            //string interpolation
+            Console.WriteLine($"Hello, {firstname} {lastname}!");
+
+
 
 
             Console.Read();
